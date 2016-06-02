b0a063057f41eec52110dd2101524d514e6f4c62
Charles Darwin Pobre <cpobre.peg@gmail.com>
2016-06-02 15:42:33 +0800

    update to class method

--- spec/lib/instagram_crawler/crawl_spec.rb
+++ spec/lib/instagram_crawler/crawl_spec.rb
@@ -4,8 +4,7 @@ require 'instagram_crawler/crawl'
 RSpec.describe InstagramCrawler::Crawl do
   describe '#call' do
     it 'should return a array of hash' do
-      crawl = InstagramCrawler::Crawl.new(url: 'https://www.instagram.com/explore/tags/wtm_tes/')
-      result = crawl.call
+      result = InstagramCrawler::Crawl.get_image_hash(url: 'https://www.instagram.com/explore/tags/wtm_tes/')
       expect(result).to be_instance_of(Array)
       result.each do |item|
         expect(item).to be_instance_of(Hash)
@@ -13,8 +12,7 @@ RSpec.describe InstagramCrawler::Crawl do
     end
 
     it 'should output HTTP empty url when url is empty' do
-      crawl = InstagramCrawler::Crawl.new
-      result = crawl.call
+      result = InstagramCrawler::Crawl.get_image_hash
       expect(result).to be_instance_of(String)
       expect(result).to eq 'HTTP request path is empty'
     end
