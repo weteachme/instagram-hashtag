8d837c5bc338462bf73ca153aa79f1e92ef0d272
Charles Darwin Pobre <cpobre.peg@gmail.com>
2016-06-03 12:35:45 +0800

    updated code

--- lib/instagram_crawler/crawl.rb
+++ lib/instagram_crawler/crawl.rb
@@ -1,29 +1,28 @@
 require 'httparty'
 require 'nokogiri'
 require 'json'
+require_relative 'json_parser'
 
 module InstagramCrawler
   class Crawl
+    extend InstagramCrawler::JSONParser
+
     def self.get_info_hash(url: '', limit: 9)
-      page = HTTParty.get(url)
-      parse_page = Nokogiri::HTML(page)
+      parse_page = get_and_parse(url)
 
-      json_matcher = 'window._sharedData = '
-      sibling = parse_page.css('#react-root').last
-
-      begin
-        sibling = sibling.next_element
-      end while sibling.text.empty? && !(sibling.text =~ /#{json_matcher}/)
-
-      inline_text = sibling.text
-
-      unparse_json = inline_text.split(/#{json_matcher}/).last.chomp(';')
-      json =  JSON.parse(unparse_json)
+      json = get_parsed_json(parse_page)
 
       data = json['entry_data']['TagPage'].first['tag']['media']['nodes'].first(limit)
       mapped_data = data.map{|post| { image: { thumb: post['thumbnail_src'], url: post['display_src'] }, code: post['code'], caption: post['caption'] } }
 
       mapped_data
     end
+
+    protected
+      class << self
+        def get_and_parse(url)
+          Nokogiri::HTML HTTParty.get(url)
+        end
+      end
   end
 end
---
+++ lib/instagram_crawler/json_parser.rb
@@ -0,0 +1,18 @@
+module InstagramCrawler
+  module JSONParser
+    protected
+      def get_parsed_json(page_content)
+        json_matcher = 'window._sharedData = '
+        sibling = page_content.css('#react-root').last
+
+        begin
+          sibling = sibling.next_element
+        end while sibling.text.empty? && !(sibling.text =~ /#{json_matcher}/)
+
+        inline_text = sibling.text
+
+        unparse_json = inline_text.split(/#{json_matcher}/).last.chomp(';')
+        JSON.parse(unparse_json)
+      end
+  end
+end
