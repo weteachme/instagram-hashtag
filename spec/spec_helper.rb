38cfd627f8b8b0f9cae13ceaf9912f44fabd4e07
Cheng Zhu <cheng@weteachme.com>
2016-06-06 14:56:38 +1000

    update spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,8 @@
+require 'codeclimate-test-reporter'
+CodeClimate::TestReporter.start
 require 'rspec'
 require 'instagram_crawler/crawl'
-require 'codeclimate-test-reporter'
 require_relative 'support/file_string'
-
-CodeClimate::TestReporter.start
 
 RSpec.configure do |config|
   config.include FileString
