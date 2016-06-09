2b0eb9c6c64c995807084335595641ccaa6e9efe
Charles Darwin Pobre <cpobre.peg@gmail.com>
2016-06-09 14:33:24 +0800

    ignore some issues

--- lib/instagram_crawler/crawl.rb
+++ lib/instagram_crawler/crawl.rb
@@ -16,7 +16,8 @@ module InstagramCrawler
 
       json = get_parsed_json(parse_page)
 
-      data = json['entry_data']['TagPage'].first['tag']['media']['nodes'].first(limit)
+      data = json['entry_data']['TagPage'].
+        first['tag']['media']['nodes'].first(limit)
 
       mapped_data = data.map do |post|
         get_hash_for_post(post)
@@ -25,21 +26,20 @@ module InstagramCrawler
       mapped_data
     end
 
-    protected
-      class << self
-        def get_hash_for_post(post)
-          { id: post['id'],
-            images: {
-              thumbnail: {
-                url: post['thumbnail_src']
-              }, standard_resolution: { url: post['display_src'] }
-            },
-            code: post['code'], caption: post['caption'], created_time: post['date'], link: LINK_URL_PREFIX + post['code'] + '/',
-            likes: post['likes'], comments: post['comments']}
-        end
-        def get_and_parse(url)
-          Nokogiri::HTML HTTParty.get(url)
-        end
+    class << self
+      def get_hash_for_post(post)
+        { 
+          id: post['id'],
+          images: {
+            thumbnail: { url: post['thumbnail_src'] }, standard_resolution: { url: post['display_src'] }
+          },
+          code: post['code'], caption: post['caption'], created_time: post['date'], link: LINK_URL_PREFIX + post['code'] + '/',
+          likes: post['likes'], comments: post['comments']
+        }
       end
+      def get_and_parse(url)
+        Nokogiri::HTML HTTParty.get(url)
+      end
+    end
   end
 end
