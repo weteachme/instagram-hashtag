2fbe8b2a02b44ba009e8581309c9347c6471d78e
Charles Darwin D.C. Pobre <cpobre.peg@gmail.com>
2017-03-22 00:15:09 +0800

    added service for instagram api

--- lib/instagram_crawler/json_parser.rb
+++ lib/instagram_crawler/json_parser.rb
@@ -6,14 +6,18 @@ module InstagramCrawler
         json_matcher = 'window._sharedData = '
         sibling = page_content.css('#react-root').last
 
-        begin
-          sibling = sibling.next_element
-        end while sibling && sibling.text.empty? && !(sibling.text =~ /#{json_matcher}/)
+        if sibling
+          begin
+            sibling = sibling.next_element
+          end while sibling && sibling.text.empty? && !(sibling.text =~ /#{json_matcher}/)
 
-        inline_text = sibling.text
+          inline_text = sibling.text
 
-        unparse_json = inline_text.split(/#{json_matcher}/).last.chomp(';')
-        JSON.parse(unparse_json)
+          unparse_json = inline_text.split(/#{json_matcher}/).last.chomp(';')
+          JSON.parse(unparse_json)
+        else
+          []
+        end
       end
   end
 end
